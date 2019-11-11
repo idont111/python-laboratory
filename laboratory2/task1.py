e50f478bda03652d89d378beb522211e57401f4a
idont111 <a.chernysh-2023@kpi.ua>
2019-11-11 23:10:06 +0200

    завдання 1

--- laboratory2/task1.py
+++ laboratory2/task1.py
@@ -2,30 +2,15 @@ print("""Черниш Аліна Андріївна\nКМ-93\nЛаборатор
 Організація циклу за допомогою оператора for""")
 """Знайти суму ( ( 2*х + 1)**і/ (х - 1)))"""
 
-import re
-
-re_integer = re.compile("^[-+]{0,1}\d+$")
-re_float = re.compile("^[-+]{0,1}\d+[.]?\d*$")
-
-def validator(pattern, promt):
-    text = input(promt)
-    while not bool(pattern.match(text)):
-        text = input(promt)
-    return text
-
-def int_validator(prompt):
-    number = int(validator(re_integer, prompt))
-    return number
-
-def float_validator(prompt):
-    number = float(validator(re_float, prompt))
-    return number
+from validators.validators_library import validator
+from validators.validators_library import re_float
+from validators.validators_library import re_integer
 
 i = 1
-n = int_validator("Введіть натуральне число: ")
+n = int(validator(re_integer,"Введіть натуральне число: "))
 while n < 1 :
-    n=int_validator("Введіть число більше за 0: ")
-x = float_validator("Введіть значення числа x: ")
+    n = int(validator(re_integer,"Введіть число більше за 0: "))
+x = float(validator(re_float,"Введіть значення числа x: "))
 while x == 1 :
     x = float_validator("Введіть число більше за 1: ")
 result=0
